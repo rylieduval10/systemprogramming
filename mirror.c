1528d316c34ef21ca8da56de8285516f9380cc9e
rylieduval10 <rylieduval10@gmail.com>
2025-02-18 19:40:27 -0600

    Update mirror.c

--- mirror.c
+++ mirror.c
@@ -1,8 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#pragma pack(1) 
-
 struct Pixel {
     unsigned char blue;
     unsigned char green;
@@ -16,7 +14,6 @@ int main() {
     origin = fopen("darthvador(1).bmp", "rb");
     new = fopen("darthvador(1)_mirrored.bmp", "wb");
 
-    // Read and write BMP header (54 bytes)
     unsigned char header[54];
     fread(header, sizeof(unsigned char), 54, origin);
     fwrite(header, sizeof(unsigned char), 54, new);
@@ -25,13 +22,10 @@ int main() {
     int width = *(int *)&header[18];
     int height = *(int *)&header[22];
 
-    // Calculate padding
     int padding = (4 - (width * 3) % 4) % 4;
 
-    // Allocate memory for a row of pixels
     struct Pixel *row = malloc(width * sizeof(struct Pixel));
 
-    // Read, mirror, and write each row
     for (int y = 0; y < height; y++) {
         fread(row, sizeof(struct Pixel), width, origin);
         fseek(origin, padding, SEEK_CUR); // Skip padding
@@ -46,7 +40,6 @@ int main() {
         }
     }
 
-    // Clean up
     free(row);
     fclose(origin);
     fclose(new);
